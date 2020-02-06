74d16e31186c16c5727a4785c78657a872c0cde5
luca hsieh <chungju.hsieh.ca@gmail.com>
2020-02-05 23:26:29 -0800

    userName renamed as CredentialId

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -56,6 +56,7 @@ export class LoginComponent implements OnInit {
           this.router.navigate([this.returnUrl]);
         },
         error => {
+          console.log(error);
           this.error = error;
           this.loading = false;
         });
