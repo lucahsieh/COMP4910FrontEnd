da6adcc368da228ed8e49315b37d1c3b80dc17fa
luca hsieh <chungju.hsieh.ca@gmail.com>
2020-02-28 23:34:30 -0800

    add dropdown of timesheet edit

--- src/app/modules/timesheets/pages/timesheet-edit/timesheet-edit.component.ts
+++ src/app/modules/timesheets/pages/timesheet-edit/timesheet-edit.component.ts
@@ -21,6 +21,8 @@ export class TimesheetEditComponent implements OnInit {
   timesheet: Timesheet = null;
   projectDropdown: SelectItem[] = null;
   employeeWPs: any[] = null;
+  projectWp: any[];
+
 
   currentUser: User = this.authenticationService.currentUserValue;
 
@@ -37,6 +39,7 @@ export class TimesheetEditComponent implements OnInit {
       console.log(`current timesheet id is  ${id}`);
       this.timesheetService.getTimesheet(id).subscribe(ts => this.timesheet = ts);
     });
+    this.prepareprojectWp();
   }
 
   onSubmit() {
@@ -53,23 +56,15 @@ export class TimesheetEditComponent implements OnInit {
   }
 
 
-  populateProjectDropdown() {
-    this.projectService.getProjectsByEmployee(this.currentUser.employeeId).subscribe(result => {
-      this.employeeWPs = [];
-      this.projectDropdown = [];
-      result.projectList.foreach(p => {
-        var projectId = p.projectId;
-        var projectName = p.projectName;
-        this.projectDropdown.push({ label: projectName, value: projectId });
-        p.workPackages.foreach(wp => {
-          var wpId = p.workPackageId;
-          var wpCode = p.workPackageCode;
-          this.employeeWPs.push(
-            { projectId: projectId, projectName: projectName, wpId: wpId, wpCode: wpCode }
-          )
-        })
-      })
-    })
+  prepareprojectWp() {
+    this.projectService.getProjectWpDropdown(this.currentUser.employeeId).subscribe(result => {
+      this.projectWp = [];
+      result.forEach(p => {
+        p.workPackages.forEach(wp => {
+          this.projectWp.push({ 'projectId': p.projectId, 'projectName': p.projectName, 'wpId': wp.workPackageId, 'wpCode': wp.workPackageCode })
+        });
+      });
+    });
   }
 
 
