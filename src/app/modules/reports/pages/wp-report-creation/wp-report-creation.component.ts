646d89e5b15435d45c7bd375f0d64ffe553303e1
luca hsieh <chungju.hsieh.ca@gmail.com>
2020-02-29 17:24:45 -0800

    createWPcreatePage

---
+++ src/app/modules/reports/pages/wp-report-creation/wp-report-creation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wp-report-creation',
+  templateUrl: './wp-report-creation.component.html',
+  styleUrls: ['./wp-report-creation.component.css']
+})
+export class WpReportCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
